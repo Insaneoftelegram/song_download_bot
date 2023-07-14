aaa4e98562c9d4f6c1135fe8ab420d487b097bdb
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-07-14 21:06:34 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -42,7 +42,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[@mwkBoTs]" 
+            performer = f"[@Resso_support]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
